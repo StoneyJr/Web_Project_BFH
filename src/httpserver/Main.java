57955e4fb88a95b068c76d7a8228de1f06c43581
Jorma Steiner <jorma.steiner@students.bfh.ch>
2023-04-02 13:23:05 +0200

    dynamic contact formular response

--- src/httpserver/Main.java
+++ src/httpserver/Main.java
@@ -11,7 +11,8 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 
 		HttpServer server = HttpServer.create(new InetSocketAddress("localhost", SERVER_PORT), 0);
-        server.createContext("/", new StaticContentHandler());
+        server.createContext("/process_contact", new ContactFormHandler());
+		server.createContext("/", new StaticContentHandler());
 		server.start();
 		System.out.println("Server ready on port " + SERVER_PORT);
 	}
---
+++ src/httpserver/handler/ContactFormHandler.java
@@ -0,0 +1,39 @@
+package httpserver.handler;
+
+import java.io.IOException;
+import java.util.Map;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import httpserver.util.Helper;
+
+public class ContactFormHandler implements HttpHandler{
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        Map<String, String> params = Helper.parseParameters(new String(exchange.getRequestBody().readAllBytes()));
+        StringBuilder formHeader = new StringBuilder();
+        StringBuilder sentForm = new StringBuilder();
+
+        formHeader.append("<h1>Vielen Dank folgene Informationen wurden von Ihnen an uns weitergeleitet:</h1>");
+        formHeader.append("<br>");
+        sentForm.append("<h2>Kontaktdaten</h2>");
+        
+        sentForm.append("<p class =\"middleInput\">");
+        
+        sentForm.append(params.get("anrede").equals("none") ? "" : (params.get("anrede") + " "));
+
+        sentForm.append(params.get("name") + " ").append(params.get("surname") + "<br>");
+        sentForm.append(params.get("mail") + "<br>");
+        sentForm.append(params.get("telefon") + "</p>");
+
+        sentForm.append("<h2>Nachricht</h2>");
+        sentForm.append("<p class =\"longInput\">" + params.get("text").replaceAll("\n", "<br>") + "</p>");
+
+        exchange.setAttribute("formHeader", formHeader);
+        exchange.setAttribute("sentForm", sentForm);
+        Helper.renderTemplate("contact.html", exchange);
+    }
+    
+}
